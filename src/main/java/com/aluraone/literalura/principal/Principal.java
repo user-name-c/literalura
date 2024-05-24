0dd662ed3246648a3acd0c1ef69ce9a821c47ec3
user-name-c <ivandgr_16@hotmail.com>
2024-05-23 20:17:32 -0600

    Añade interaccion con el usuario

--- src/main/java/com/aluraone/literalura/principal/Principal.java
+++ src/main/java/com/aluraone/literalura/principal/Principal.java
@@ -4,20 +4,54 @@ import com.aluraone.literalura.model.Datos;
 import com.aluraone.literalura.service.ConsumoAPI;
 import com.aluraone.literalura.service.ConvierteDatos;
 
+import java.util.Scanner;
+
+
 public class Principal {
+    private static final String URL_BASE = "https://gutendex.com/books/";
     private ConvierteDatos conversor = new ConvierteDatos();
     private ConsumoAPI consulta = new ConsumoAPI();
-    private static final String URL_BASE = "https://gutendex.com/books/";
+    private int opcionUsuario = -1;
+
+    Scanner teclado = new Scanner(System.in);
 
     public void consultaEjemplo(){
 
-        String busqueda = "?search="+"el+quijote";
+        do{
+            mostrarMenu();
+            opcionUsuario = Integer.valueOf(teclado.nextLine());
+            switch(opcionUsuario){
+                case 1:
+                    System.out.println("Ingrese el nombre del libro que desea buscar");
+                    String libroUsuario = teclado.nextLine();
+                    String busqueda = "?search=" + libroUsuario.replace(" ","+");
+                    var json = consulta.obtenerDatos(URL_BASE + busqueda);
+                    System.out.println(json);
+                    var datos = conversor.obtenerDatos(json, Datos.class);
+                    System.out.println(datos);
+                    break;
+                case 2:
+                    System.out.println( "Opcion aun no implementada");
+                    break;
+                case 0:
+                    System.out.println("Finalizando el programa");
+                    break;
+                default:
+                    System.out.println("Opcion no valida");
+            }
+        } while  (opcionUsuario != 0);
 
-        var json = consulta.obtenerDatos(URL_BASE + busqueda);
-        System.out.println(json);
-        var datos = conversor.obtenerDatos(json, Datos.class);
-        System.out.println(datos);
 
+    }
+
+    public void mostrarMenu(){
+        System.out.println("""
+                Selecciones una opción             
+                1- buscar libro por titulo
+                2- Listar libros registrados
+                
+                0- Salir
+                """);
     }
 
 
